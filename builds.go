0f4b70c82f0cfd59c1ef89435a949dbfc84732cb
Roman Tishchenko <rtishchenko@guidewire.com>
2017-07-21 16:40:01 +0200

    Add pagination support

--- builds.go
+++ builds.go
@@ -2,15 +2,14 @@ package teamcity
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/fatih/structs"
+	"github.com/sethgrid/pester"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
 	"strings"
-	// "fmt"
 	"unicode"
-	"github.com/fatih/structs"
-	"github.com/sethgrid/pester"
-	// "errors"
 )
 
 func New(host, username, password string) *Client {
@@ -30,12 +29,19 @@ func New(host, username, password string) *Client {
 	return client
 }
 
+func NewBuildLocator() *BuildLocator {
+	return &BuildLocator{
+		Branch: "default:any",
+		Count:  "1",
+	}
+}
+
 func processParams(str interface{}) string {
 	conv := structs.Map(str)
 	res := ""
 
 	for k, v := range conv {
-		if v == ""  {
+		if v == "" {
 			continue
 		}
 		fieldName := []rune(string(k))
@@ -43,35 +49,28 @@ func processParams(str interface{}) string {
 		res += "," + string(fieldName) + ":" + v.(string)
 	}
 
-	//debug
-	// fmt.Println("Trimmed line: ", strings.TrimLeft(res, ","))
-
 	return strings.TrimLeft(res, ",")
 }
 
-func (c *Client) GetBuildStatistics(bl BuildLocator) (BuildStatistics, error) {
-  statistics := BuildStatistics{}
-	statistics.UsedFilter = bl
+func (c *Client) GetBuildStat(id int) (BuildStatistics, error) {
+	statistics := BuildStatistics{}
+	url := fmt.Sprint(c.host, "/app/rest/builds/id:", id, "/statistics")
 
-	url := c.host + "/app/rest/builds/" + processParams(bl) + "/statistics"
-	// fmt.Printf("url %s\n", url)
 	req, err := http.NewRequest("GET", url, nil)
-  if err != nil {
-    return statistics, err
-  }
+	if err != nil {
+		return statistics, err
+	}
 	req.Header.Add("Accept", "application/json")
 	res, err := c.HTTPClient.Do(req)
-  if err != nil {
-    return statistics, err
-  }
+	if err != nil {
+		return statistics, err
+	}
 	if res.StatusCode == 401 {
-		// debug
-		// log.Println("Add basic auth to GetBuildStatistics")
 		req.SetBasicAuth(c.username, c.password)
 		res, err = c.HTTPClient.Do(req)
-    if err != nil {
-      return statistics, err
-    }
+		if err != nil {
+			return statistics, err
+		}
 	}
 
 	if res.StatusCode == 404 {
@@ -80,72 +79,65 @@ func (c *Client) GetBuildStatistics(bl BuildLocator) (BuildStatistics, error) {
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-	// fmt.Println(string(body))
-  if err != nil {
-    return statistics, err
-  }
-
-	// debug
-	// fmt.Println(res)
-	// fmt.Println(string(body))
+	if err != nil {
+		return statistics, err
+	}
 
 	if err := json.Unmarshal(body, &statistics); err != nil {
 		return statistics, err
 	}
 
-	// debug
-	// fmt.Println("\n")
-	// fmt.Println(statistics)
-  return statistics, nil
+	return statistics, nil
 }
 
-func (c *Client) GetBuildsByParams(btl BuildTypeLocator, bl BuildLocator) (Builds, error) {
-  statistics := Builds{}
+func (c *Client) GetBuildsByParams(bl BuildLocator) (Builds, error) {
+	result := Builds{}
+	url := c.host + "/app/rest/builds/?locator=" + processParams(bl)
 
-  url := c.host + "/app/rest/buildTypes/" + processParams(btl) + "/builds/" + processParams(bl)
-	req, err := http.NewRequest("GET", url, nil)
-  if err != nil {
-    return statistics, err
-  }
-	req.Header.Add("Accept", "application/json")
-	res, err := c.HTTPClient.Do(req)
-  if err != nil {
-    return statistics, err
-  }
-	if res.StatusCode == 401 {
-		// debug
-		// log.Println("Add basic auth to GetBuildsByParams")
-		req.SetBasicAuth(c.username, c.password)
-		res, err = c.HTTPClient.Do(req)
-    if err != nil {
-      return statistics, err
-    }
+	for {
+		statistics := Builds{}
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return statistics, err
+		}
+		req.Header.Add("Accept", "application/json")
+		res, err := c.HTTPClient.Do(req)
+		if err != nil {
+			return statistics, err
+		}
+		if res.StatusCode == 401 {
+			req.SetBasicAuth(c.username, c.password)
+			res, err = c.HTTPClient.Do(req)
+			if err != nil {
+				return statistics, err
+			}
+		}
+
+		if res.StatusCode == 404 {
+			return statistics, nil
+		}
+
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return statistics, err
+		}
+
+		if err := json.Unmarshal(body, &statistics); err != nil {
+			return statistics, err
+		}
+
+		for i := range statistics.Build {
+			result.Build = append(result.Build, statistics.Build[i])
+		}
+
+		if statistics.NextHref != "" {
+			url = c.host + statistics.NextHref
+		} else {
+			break
+		}
 	}
-
-	if res.StatusCode == 404 {
-		return statistics, nil
-	}
-
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	// fmt.Println(string(body))
-  if err != nil {
-    return statistics, err
-  }
-
-	// debug
-	// fmt.Println(res)
-	// fmt.Println(string(body))
-
-	if err := json.Unmarshal(body, &statistics); err != nil {
-		return statistics, err
-	}
-
-	// debug
-	// fmt.Println("\n")
-	// fmt.Println(statistics)
-
-  return statistics, nil
+	return result, nil
 }
 
 func (c *Client) GetAllBuildConfigurations() (BuildConfigurations, error) {
@@ -154,21 +146,19 @@ func (c *Client) GetAllBuildConfigurations() (BuildConfigurations, error) {
 	url := c.host + "/app/rest/buildTypes?="
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-    return statistics, err
-  }
+		return statistics, err
+	}
 	req.Header.Add("Accept", "application/json")
 	res, err := c.HTTPClient.Do(req)
-  if err != nil {
-    return statistics, err
-  }
+	if err != nil {
+		return statistics, err
+	}
 	if res.StatusCode == 401 {
-		// debug
-		// log.Println("Add basic auth to GetBuildsByParams")
 		req.SetBasicAuth(c.username, c.password)
 		res, err = c.HTTPClient.Do(req)
-    if err != nil {
-      return statistics, err
-    }
+		if err != nil {
+			return statistics, err
+		}
 	}
 
 	if res.StatusCode == 404 {
@@ -177,22 +167,14 @@ func (c *Client) GetAllBuildConfigurations() (BuildConfigurations, error) {
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    return statistics, err
-  }
-
-	// debug
-	// fmt.Println(res)
-	// fmt.Println(string(body))
+	if err != nil {
+		return statistics, err
+	}
 
 	if err := json.Unmarshal(body, &statistics); err != nil {
 		return statistics, err
 	}
 
-	// debug
-	// fmt.Println("\n")
-	// fmt.Println(statistics)
-
-  return statistics, nil
+	return statistics, nil
 
 }
--- types.go
+++ types.go
@@ -18,10 +18,6 @@ type BuildLocator struct {
 	Count     string `yaml:"count"`
 }
 
-type BuildTypeLocator struct {
-	Id string
-}
-
 type BuildStatistics struct {
 	Count    int `json:"count"`
 	Property []struct {
@@ -31,126 +27,22 @@ type BuildStatistics struct {
 	UsedFilter BuildLocator
 }
 
+type Build struct {
+	ID          int    `json:"id"`
+	BuildTypeID string `json:"buildTypeId"`
+	Number      string `json:"number"`
+	Status      string `json:"status"`
+	State       string `json:"state"`
+	BranchName  string `json:"branchName"`
+	Href        string `json:"href"`
+	WebURL      string `json:"webUrl"`
+}
+
 type Builds struct {
-	ID            int    `json:"id"`
-	BuildTypeID   string `json:"buildTypeId"`
-	Number        string `json:"number"`
-	Status        string `json:"status"`
-	State         string `json:"state"`
-	BranchName    string `json:"branchName"`
-	DefaultBranch bool   `json:"defaultBranch"`
-	Href          string `json:"href"`
-	WebURL        string `json:"webUrl"`
-	StatusText    string `json:"statusText"`
-	BuildType     struct {
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		Description string `json:"description"`
-		ProjectName string `json:"projectName"`
-		ProjectID   string `json:"projectId"`
-		Href        string `json:"href"`
-		WebURL      string `json:"webUrl"`
-	} `json:"buildType"`
-	QueuedDate string `json:"queuedDate"`
-	StartDate  string `json:"startDate"`
-	FinishDate string `json:"finishDate"`
-	Triggered  struct {
-		Type    string `json:"type"`
-		Details string `json:"details"`
-		Date    string `json:"date"`
-	} `json:"triggered"`
-	LastChanges struct {
-		Change []struct {
-			ID       int    `json:"id"`
-			Version  string `json:"version"`
-			Username string `json:"username"`
-			Date     string `json:"date"`
-			Href     string `json:"href"`
-			WebURL   string `json:"webUrl"`
-		} `json:"change"`
-		Count int `json:"count"`
-	} `json:"lastChanges"`
-	Changes struct {
-		Href string `json:"href"`
-	} `json:"changes"`
-	Revisions struct {
-		Count    int `json:"count"`
-		Revision []struct {
-			Version         string `json:"version"`
-			VcsBranchName   string `json:"vcsBranchName"`
-			VcsRootInstance struct {
-				ID        string `json:"id"`
-				VcsRootID string `json:"vcs-root-id"`
-				Name      string `json:"name"`
-				Href      string `json:"href"`
-			} `json:"vcs-root-instance"`
-		} `json:"revision"`
-	} `json:"revisions"`
-	Agent struct {
-		ID     int    `json:"id"`
-		Name   string `json:"name"`
-		TypeID int    `json:"typeId"`
-		Href   string `json:"href"`
-	} `json:"agent"`
-	TestOccurrences struct {
-		Count     int    `json:"count"`
-		Href      string `json:"href"`
-		Default   bool   `json:"default"`
-		Passed    int    `json:"passed"`
-		Failed    int    `json:"failed"`
-		NewFailed int    `json:"newFailed"`
-		Ignored   int    `json:"ignored"`
-	} `json:"testOccurrences"`
-	ProblemOccurrences struct {
-		Count   int    `json:"count"`
-		Href    string `json:"href"`
-		Default bool   `json:"default"`
-	} `json:"problemOccurrences"`
-	Artifacts struct {
-		Href string `json:"href"`
-	} `json:"artifacts"`
-	RelatedIssues struct {
-		Href string `json:"href"`
-	} `json:"relatedIssues"`
-	Properties struct {
-		Count    int `json:"count"`
-		Property []struct {
-			Name      string `json:"name"`
-			Value     string `json:"value"`
-			Inherited bool   `json:"inherited"`
-		} `json:"property"`
-	} `json:"properties"`
-	Statistics struct {
-		Href string `json:"href"`
-	} `json:"statistics"`
-	SnapshotDependencies struct {
-		Count int `json:"count"`
-		Build []struct {
-			ID            int    `json:"id"`
-			BuildTypeID   string `json:"buildTypeId"`
-			Number        string `json:"number"`
-			Status        string `json:"status"`
-			State         string `json:"state"`
-			BranchName    string `json:"branchName"`
-			DefaultBranch bool   `json:"defaultBranch"`
-			Href          string `json:"href"`
-			WebURL        string `json:"webUrl"`
-		} `json:"build"`
-	} `json:"snapshot-dependencies"`
-	ArtifactDependencies struct {
-		Count int `json:"count"`
-		Build []struct {
-			ID            int    `json:"id"`
-			BuildTypeID   string `json:"buildTypeId"`
-			Number        string `json:"number"`
-			Status        string `json:"status"`
-			State         string `json:"state"`
-			BranchName    string `json:"branchName"`
-			DefaultBranch bool   `json:"defaultBranch"`
-			Href          string `json:"href"`
-			WebURL        string `json:"webUrl"`
-		} `json:"build"`
-	} `json:"artifact-dependencies"`
+	Count    int     `json:"count"`
+	Href     string  `json:"href"`
+	NextHref string  `json:"nextHref"`
+	Build    []Build `json:"build"`
 }
 
 type BuildConfigurations struct {
